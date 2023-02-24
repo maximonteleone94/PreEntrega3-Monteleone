ca5a966683eba4edc1365381c307ca1e6e79dace
MaximilianoMonteleone <meganecupe94@hotmail.com>
2023-02-24 15:43:54 -0300

    proy1

--- main.js
+++ main.js
@@ -1,29 +1,22 @@
 const  suma  =  ( num1 ,  num2 )  =>  num1  +  num2 ;
 const  resta  =  ( num1 ,  num2 )  =>  num1  -  num2 ;
-const  envio  =  ( envio )  =>  500 ;
 
-
-
+for (let entrada = 1; entrada <= 1; entrada++){
+    let nombre = prompt ("Ingrese patente de vehículo a lavar");
+    alert(`Turno #${entrada} Patente: ${nombre}`);
+}
 let  precioProducto =  parseInt ( prompt ( "Ingrese el monto de compra" ) ) ;
 if(precioProducto >= 1000){
   alert("Usted tiene una bonificacion");
 }
 else if(precioProducto <= 1000){
-  alert("Usted tiene envío gratis")
+  alert("Usted tiene un perfume gratis")
 }
 let  tiketDescuento = parseInt ( prompt ("Ingrese el codigo de su tiket"));
 let  descuentoCompra  =  parseInt ( prompt ( "Ingrese su descuento" ) ) ;
-let  totalEnvio  =  envio ( envio ) ;
-let  sumaConEnvio  =  suma ( precioProducto ,  totalEnvio ) ;
+let  nuevoPrecio  =  resta ( precioProducto, descuentoCompra ) ;
 
-let  nuevoPrecio  =  resta ( sumaConEnvio ,  descuentoCompra ) ;
-
-let  mensaje  =  `Total con envio ${ nuevoPrecio } ` ;
+let  mensaje  =  `Total ${ nuevoPrecio } ` ;
 alert ( mensaje ) ;
 
-
-for (let entrada = 1; entrada <= 2; entrada++){
-    let nombre = prompt ("Ingrese patente de vehículo a lavar");
-    alert(`Turno #${entrada} Patente: ${nombre}`);
-}
     alert ("Turnos de lavados agotados");
